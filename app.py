608fd316cc0fe669abb1de3e68f182ef5b7bf353
hecarde2 <hectordico@hotmail.com>
2026-03-25 07:12:36 -0500

    Update app.py

--- app.py
+++ app.py
@@ -6,14 +6,14 @@ print("\nBIENVENIDO/A AL SISTEMA DE INVENTARIO")
 inventario = []
 option = 0
 
-# -------------------------------------------------
-# MENÚ PRINCIPAL
-# -------------------------------------------------
+
+# MENÚ PRINCIPAL, OPCION 9 PARA SALIR
+
 while option != 9:
 
     mostrar_menu()
 
-    # -------- VALIDAR OPCIÓN --------
+    # SI HAY ALGÚN ERROR DE UN NÚMERO FUERA DE RANGO, SE REPITA EL MENÚ Y EL PROGRAMA SIGA
     try:
         option = int(input("Ingrese una opción (1-9): "))
 
@@ -26,67 +26,51 @@ while option != 9:
         continue
 
 
-    # -------------------------------------------------
-    # 1. AGREGAR PRODUCTO
-    # -------------------------------------------------
+    # FUNCIONES DEFINIDAS CON LAS OPCIONES DEL MENÚ 
+
     if option == 1:
         agregar_producto(inventario)
 
 
-    # -------------------------------------------------
-    # 2. MOSTRAR INVENTARIO
-    # -------------------------------------------------
+    
     elif option == 2:
         mostrar_inventario(inventario)
 
 
-    # -------------------------------------------------
-    # 3. BUSCAR PRODUCTO
-    # -------------------------------------------------
+   
     elif option == 3:
         buscar_producto(inventario)
 
 
-    # -------------------------------------------------
-    # 4. ACTUALIZAR PRODUCTO
-    # -------------------------------------------------
+   
     elif option == 4:
         actualizar_producto(inventario)
 
 
-    # -------------------------------------------------
-    # 5. ELIMINAR PRODUCTO
-    # -------------------------------------------------
     elif option == 5:
         eliminar_producto(inventario)
 
 
-    # -------------------------------------------------
-    # 6. ESTADÍSTICAS
-    # -------------------------------------------------
     elif option == 6:
         calcular_estadisticas(inventario)
 
 
-    # -------------------------------------------------
-    # 7. GUARDAR CSV
-    # -------------------------------------------------
+    #  GUARDAR CSV
+
     elif option == 7:
         ruta = input("Nombre del archivo (ej: inventario.csv): ")
         guardar_csv(inventario, ruta)
 
 
-    # -------------------------------------------------
-    # 8. CARGAR CSV
-    # -------------------------------------------------
+    #  CARGAR CSV
+
     elif option == 8:
         ruta = input("Archivo a cargar: ")
         inventario = cargar_csv(ruta, inventario)
 
 
-    # -------------------------------------------------
-    # 9. SALIR
-    # -------------------------------------------------
+
+
     elif option == 9:
         print("HASTA PRONTO")
         break
